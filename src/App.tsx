fc7b5c1af2564bbcb206f47e3f1213c61790cd4f
sarahmartin897 <sarahmartin024@gmail.com>
2025-05-26 15:59:42 -0400

    remove lorem ipsum

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,7 @@ function App() {
         <h1 className="mb-2 text-lg sm:text-2xl md:text-4xl lg:text-7xl">
           Sarahs Portfolio
         </h1>
-        <div className="flex flex-col gap-4 text-zinc-500 text-sm sm:text-base md:text-lg lg:text-xl font-mono">
-          <p>
-            I build clean, responsive, and user-friendly web interfaces using
-            HTML, CSS, JavaScript, and React. I care deeply about good design,
-            performance, and accessibility, and I enjoy turning complex problems
-            into simple, intuitive solutions.
-          </p>
-          <p>
-            Skilled in: React, JavaScript (ES6+), CSS3, HTML5, Tailwind, Git,
-            Figma, and REST APIs.
-          </p>
-          <p>Always learning, always building, let’s connect!</p>
-        </div>
+       
       </div>
     </div>
   );
