b427742fa9903a9a14c2664b0d1b46b61509dc14
hartmandy <120344775+hartmandy@users.noreply.github.com>
2025-05-26 14:32:25 -0400

    add shadow

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="container mx-auto h-screen flex items-center justify-center px-2">
-      <div className="font-medium border p-4 inline-block rounded-xl border-zinc-200 bg-zinc-50">
+      <div className="font-medium border p-4 inline-block rounded-xl border-zinc-200 bg-zinc-50 shadow-lg">
         <h1 className="mb-2 text-lg sm:text-2xl md:text-4xl lg:text-7xl">
           Sarahs Portfolio
         </h1>
